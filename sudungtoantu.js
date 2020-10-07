0a7e66d890aadcbff6900165340a50a180f61271
quockunss <72240212+quockunss@users.noreply.github.com>
2020-10-07 11:51:35 +0700

    add sudungtoantu.html

---
+++ sudungtoantu.js
@@ -0,0 +1,8 @@
+var inputwidth;
+var inputheight;
+inputwidth = prompt('width:');
+inputheight = prompt('height');
+var width = parseInt(inputwidth);
+var height = parseInt(inputheight);
+var area = width * height;
+document.write('area:' + area);
